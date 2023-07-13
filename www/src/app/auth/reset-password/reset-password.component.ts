6bf5cc6cca12f03cc0929538b9b20eb62cf2463a
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-07-13 15:17:11 +0200

    add reset page

---
+++ www/src/app/auth/reset-password/reset-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bra-reset-password',
+  templateUrl: './reset-password.component.html',
+  styles: [
+  ]
+})
+export class ResetPasswordComponent {
+
+}
