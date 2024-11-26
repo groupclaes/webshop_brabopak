cf3acb0791d21eaf9ac3369ccc6927fab159817e
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2024-11-26 15:44:08 +0100

    update core

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component'
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { LayoutModule } from './@shared/layout/layout.module'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { AuthInterceptor } from './auth/auth.interceptor'
 import { AtSharedModule } from './@shared/@shared.module'
 import { HttpLoaderFactory } from './@shared/loaders/translate-browser.loader'
@@ -19,43 +19,37 @@ export function localizeLoaderFactory(translate: TranslateService, location: Loc
   return new ManualParserLoader(translate, location, settings, environment.supportedLanguages.map(e => e.split('-')[0]), 'ROUTES.', '!')
 }
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
-    HttpClientModule,
-    AtSharedModule,
-    LayoutModule,
-    TranslateModule.forRoot({
-      useDefaultLang: true,
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-    LocalizeRouterModule.forRoot([...routes], {
-      useCachedLang: false,
-      parser: {
-        provide: LocalizeParser,
-        useFactory: localizeLoaderFactory,
-        deps: [TranslateService, Location, LocalizeRouterSettings]
-      }
-    }),
-    NoopAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        AppRoutingModule,
+        AtSharedModule,
+        LayoutModule,
+        TranslateModule.forRoot({
+            useDefaultLang: true,
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
+        LocalizeRouterModule.forRoot([...routes], {
+            useCachedLang: false,
+            parser: {
+                provide: LocalizeParser,
+                useFactory: localizeLoaderFactory,
+                deps: [TranslateService, Location, LocalizeRouterSettings]
+            }
+        }),
+        NoopAnimationsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
